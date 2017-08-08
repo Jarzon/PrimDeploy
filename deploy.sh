bbf7970426a38d0595e13bed7e2b2716a130e70e
Jarzon <j@masterj.net>
2017-08-08 17:22:29 -0400

    Add code to deploy the latest tagged version

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,26 @@ if [ ! -d "./app/deploy" ]; then
     exit 1
 fi
 
-formatEcho "Sending files to the prod server : $ssh_server"
+# TODO: get the first command param to deploy to that version instead of the latest
+
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+
+formatEcho "Stashing local changes"
+git stash
+
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+formatEcho "Checkout project to latest version: $latestTag"
+git checkout $latestTag
+
+formatEcho "Sending files to the prod server: $ssh_server"
 rsync --delete --compress --times --recursive --verbose --copy-links --exclude-from './app/deploy/exclude.txt' ./* $ssh_user@$ssh_server:$root_dir/$name/htdocs/
+
+formatEcho "Checkout project back to latest branch: $currentBranch"
+git checkout $currentBranch
+
+formatEcho "Unstashing latest shash"
+git stash apply
 
 formatEcho "Create a backup of the database before the migration."
 ssh $ssh_user@$ssh_server "cd $root_dir/$name && bash -s" < ../primdeploy/bashScripts/backup.sh
