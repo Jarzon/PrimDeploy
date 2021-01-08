6eabf59ded55f2a13330f0ce97c4ff7bea2076b3
jarzon <j@jasonvaillancourt.ca>
2021-01-08 16:09:33 -0500

    Fix rsync not sending hidden folders

--- deploy.sh
+++ deploy.sh
@@ -53,7 +53,7 @@ formatEcho "Auth on server: $ssh_server"
 ssh -t $ssh_user@$ssh_server 'sudo -v'
 
 formatEcho "Sending files to the $deployenvironement server: $ssh_server"
-rsync --compress --times --recursive --verbose --delete --perms --owner --group --copy-links --exclude-from './app/deploy/exclude.txt' -e 'ssh' '--rsync-path=sudo rsync' ./* $ssh_user@$ssh_server:$root_dir/$name/$deployfolder/
+rsync --compress --times --recursive --verbose --delete --perms --owner --group --copy-links --exclude-from './app/deploy/exclude.txt' -e 'ssh' '--rsync-path=sudo rsync' ./ $ssh_user@$ssh_server:$root_dir/$name/$deployfolder/
 
 if [[ $deployenvironement = 'prod' ]]; then
     formatEcho "Checkout project back to latest branch: $currentBranch"
