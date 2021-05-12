993ef2ee79441f636a3f4cd5a27d6542ad2982a2
jarzon <j@jasonvaillancourt.ca>
2021-05-12 09:48:44 -0400

    Add folder perms

--- bashScripts/init.sh
+++ bashScripts/init.sh
@@ -2,15 +2,15 @@
 . ./htdocs/app/deploy/config.sh
 
 #TODO: Create a user for the project
-sudo chown -R www-data:www-data ./htdocs # TODO: Change the user from a project based user
+sudo chown -R www-data:www-data ./htdocs
 sudo chmod 755 ./htdocs/
-# TODO: Add perms for logs folder?
-# TODO: Add chown for logs folder?
+sudo chmod -R 7770 ./htdocs/data/
+sudo chmod -R 7770 ./htdocs/app/cache/
+sudo chmod -R 7770 ./htdocs/public/uploads/
 
 sudo formatEcho "Create databases"
 
 sudo mysql --password=$password < ./htdocs/app/deploy/databases.sql
-#TODO: Create a mysql user for the project
 
 formatEcho "Enable project's vhost"
 sudo a2ensite $name
